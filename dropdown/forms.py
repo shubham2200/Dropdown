17e261e65f8b814bc08a9072a7b1c23ea5a93524
Your Name <you@example.com>
2021-11-25 23:48:45 +0530

    yes

--- dropdown/forms.py
+++ dropdown/forms.py
@@ -9,15 +9,4 @@ class PersonCreationForm(forms.ModelForm):
         fields = '__all__'
 
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['state'].queryset = state.objects.all()
-
-        if 'Country' in self.data:
-            try:
-                Country_id = int(self.data.get('Country'))
-                self.fields['state'].queryset = state.objects.filter(Country_id=Country_id).order_by('name')
-            except (ValueError, TypeError):
-                pass  # invalid input from the client; ignore and fallback to empty City queryset
-      #  elif self.instance.pk:
-          #  self.fields['state'].queryset = self.instance.Country.state_set.order_by('name')
+  
